413d8481eff645292437bfbcac696ca35e86a9e1
Sam Penders <sam.penders10@gmail.com>
2021-03-29 21:28:54 -0500

    removed files from index

--- check_failed.sh
+++
@@ -1,1 +0,0 @@
-echo "select * from photos where dont_post=1 order by posted_date;" | sqlite3 photoDB.db
--- test.py
+++
@@ -1,8 +0,0 @@
-from send_tweet import *
-from random import randint
-
-db = photoDB('photoDB.db')
-r = db.get_random_row('test')
-print(r)
-
-db.con.close()
